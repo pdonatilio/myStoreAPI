deb4f99b47022c123f29bd478b92b50bf0fe629a
pdonatilio <pdonatilio@gmail.com>
2018-08-19 16:06:55 -0300

    Adding the Pomero package in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace MyStoreApi
 {
@@ -24,7 +25,7 @@ namespace MyStoreApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
